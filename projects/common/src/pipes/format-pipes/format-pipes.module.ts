7484fcde01b49786cd886fe5d1018eeb7122d6f7
Revin Roman <xgismox@gmail.com>
2019-10-22 11:45:39 +0300

    Remove phone format pipe

--- projects/common/src/pipes/format-pipes/format-pipes.module.ts
+++ projects/common/src/pipes/format-pipes/format-pipes.module.ts
@@ -3,11 +3,10 @@ import { HighlightPipe } from './highlight.pipe';
 import { MomentPipe } from './moment.pipe';
 import { MoneyPipe } from './money.pipe';
 import { NumberPipe } from './number.pipe';
-import { PhonePipe } from './phone.pipe';
 import { PluralPipe } from './plural.pipe';
 import { SrcsetPipe } from './srcset.pipe';
 
-const pipes = [HighlightPipe, MomentPipe, MoneyPipe, NumberPipe, PhonePipe, PluralPipe, SrcsetPipe];
+const pipes = [HighlightPipe, MomentPipe, MoneyPipe, NumberPipe, PluralPipe, SrcsetPipe];
 
 @NgModule({
   declarations: pipes,
--- projects/common/src/pipes/format-pipes/index.ts
+++ projects/common/src/pipes/format-pipes/index.ts
@@ -2,6 +2,5 @@ export * from './highlight.pipe';
 export * from './moment.pipe';
 export * from './money.pipe';
 export * from './number.pipe';
-export * from './phone.pipe';
 export * from './plural.pipe';
 export * from './srcset.pipe';
