9d023609e5b650dce7956609ff3179659f05fb51
Revin Roman <xgismox@gmail.com>
2020-10-01 16:39:14 +0300

    Update

--- projects/common/src/functions/debugFn.ts
+++ projects/common/src/functions/debugFn.ts
@@ -1,3 +1,3 @@
 import { Debug } from 'debug';
 
-export const debugFn: Debug = require('debug/dist/debug.js');
+export const debugFn: Debug = require('debug/src/index.js');
--- projects/moment/src/functions/utc-date.ts
+++ projects/moment/src/functions/utc-date.ts
@@ -1,8 +1,9 @@
 import * as _momentTz from 'moment-timezone';
+import { Moment, MomentFormatSpecification, MomentInput } from 'moment-timezone';
 
 const moment = _momentTz;
 
-export function dateToUtc (date: _momentTz.Moment | Date | string, format?: _momentTz.MomentFormatSpecification): _momentTz.Moment | null {
+export function dateToUtc (date: MomentInput, format?: MomentFormatSpecification): Moment | null {
   if (!date) {
     return null;
   }
@@ -10,7 +11,7 @@ export function dateToUtc (date: _momentTz.Moment | Date | string, format?: _mom
   return moment(date, format).utcOffset(0, true);
 }
 
-export function dateFromUtc (date: _momentTz.Moment | Date | string, format?: _momentTz.MomentFormatSpecification): _momentTz.Moment | null {
+export function dateFromUtc (date: MomentInput, format?: MomentFormatSpecification): Moment | null {
   if (!date) {
     return null;
   }
